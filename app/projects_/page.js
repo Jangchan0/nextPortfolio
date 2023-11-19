362dab32869ebae927d6d3558c47cd1edb1a9cd1
Jangchanyoung <jchy16@gmail.com>
2023-11-20 00:37:07 +0900

    FEAT: 노션 API 프로젝트 이름 가져오기 성공!

--- app/projects_/page.js
+++ app/projects_/page.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import { TOKEN, DATABASE_ID } from '../../config/index';
 
 const Projects = () => {
+    async function getStaticProps() {
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'Notion-Version': '2022-06-28',
+                'content-type': 'application/json',
+                authorization: `Bearer ${TOKEN}`,
+            },
+            body: JSON.stringify({ page_size: 100 }),
+        };
+
+        const res = await fetch(
+            `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+            options
+        );
+        const projects = await res.json();
+
+        const projectsName = projects.results.map((aProject) => {
+            return aProject.properties.이름.title[0].text.content;
+        });
+
+        console.log(`project : ${projectsName}`);
+
+        return {
+            props: {},
+        };
+    }
+    getStaticProps();
+
     return <h1>프로젝트</h1>;
 };
 
 export default Projects;
-
-export async function getStaticProps() {
-    const options = {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'Notion-Version': '2022-06-28',
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({ filter: 'string', start_cursor: 'string', page_size: 100 }),
-    };
-
-    fetch('https://api.notion.com/v1/databases/database_id/query', options)
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
-
-    return {
-        props: {},
-    };
-}
