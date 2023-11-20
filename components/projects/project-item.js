000d717db3426a98a37b2e4dba6d50b9c169f3e4
Jangchanyoung <jchy16@gmail.com>
2023-11-21 04:51:28 +0900

    FEAT: Github 링크 추가 중(project-item.js)

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -1,9 +1,13 @@
 export default function ProjectItem(data) {
-    const projectTitle = data.data.properties.이름.title[0]?.text.content;
+    console.log(data);
+
+    const title = data.data.properties.이름.title[0]?.text.content;
+    const progressPeriod = data.data.properties[`진행 기간`].date;
+    const githubLink = data.data.properties;
 
     return (
         <div className="p-6 bg-slate-400 rounded-md">
-            <h1>{projectTitle}</h1>
+            <h1>{title}</h1>
         </div>
     );
 }
