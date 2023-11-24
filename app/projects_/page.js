dd57f89b20c5f8c591f460bcc5fb031a47329c1b
Jangchanyoung <jchy16@gmail.com>
2023-11-24 23:35:52 +0900

    MODIFY: 프로젝트 갯수 타이틀 색상 변경

--- app/projects_/page.js
+++ app/projects_/page.js
@@ -19,7 +19,9 @@ export default async function Projects() {
 
     return (
         <>
-            <h1 className="text-4xl font-bold sm:text-6xl">총 프로젝트 : {data.results.length}</h1>
+            <h1 className="text-4xl font-bold sm:text-6xl">
+                총 프로젝트 : <span className="pl-4 text-blue-500">{data.results.length}</span>{' '}
+            </h1>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-6 py-10 sm:w-full">
                 {projectSortByDate.map((aProject) => {
