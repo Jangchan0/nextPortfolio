7a9536fe4a89e3bc305fc7a9a522aa9eef3abc37
Jangchanyoung <jchy16@gmail.com>
2023-11-25 06:12:28 +0900

    FEAT: 노션 링크 추가, 프로젝트 박스 크기 조정

--- app/projects_/page.js
+++ app/projects_/page.js
@@ -19,11 +19,14 @@ export default async function Projects() {
 
     return (
         <>
-            <h1 className="text-4xl font-bold sm:text-6xl">
-                총 프로젝트 : <span className="pl-4 text-blue-500">{data.results.length}</span>{' '}
+            <h1 className="text-4xl font-bold sm:text-6xl justify-center flex">
+                총 프로젝트 : <span className="pl-4 text-blue-500">{data.results.length}</span>
             </h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-6 py-10 sm:w-full">
+            <div
+                className="grid grid-cols-1 md:grid-cols-2 gap-8 m-6 flex  py-10 "
+                style={{ justifyItems: 'center', alignItems: 'center' }}
+            >
                 {projectSortByDate.map((aProject) => {
                     return <ProjectItem key={aProject.id} data={aProject} />;
                 })}
--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ProjectItem(data) {
     const title = data.data.properties.이름.title[0]?.text.content;
@@ -9,27 +10,59 @@ export default function ProjectItem(data) {
     const githubLink = data.data.properties.Github.url;
     const skillList = data.data.properties.Skills.multi_select;
     const imgSrc = data.data.cover.file.url;
+    const notionDetail = data.data.public_url;
 
     const discription = discriptionList.map((item) => item.plain_text);
-    const skill = skillList.map((item) => item.name);
 
     return (
-        <div className="flex flex-col m-3 bg-slate-700 rounded-xl">
-            <Image
-                className="rounded-t-xl"
-                src={imgSrc}
-                alt="cover image"
-                width={500}
-                height={500}
-                layout="responsive"
-                objectFit="contain"
-                quality={100}
-            />
-
-            <h1>{title}</h1>
-            <p>{skill}</p>
-            <h3>{discription}</h3>
-            <a href={githubLink}>깃허브 바로가기</a>
+        <div className="project-card  w-[35vw] min-w-[340px] ">
+            <Link href={notionDetail}>
+                <Image
+                    className="rounded-t-xl "
+                    src={imgSrc}
+                    alt="cover image"
+                    width={50}
+                    height={50}
+                    layout="responsive"
+                    objectFit="contain"
+                    quality={100}
+                />
+            </Link>
+            <div className="p-4 flex flex-col">
+                <Link href={notionDetail}>
+                    <h1 className="text-2xl font-bold">{title}</h1>
+                    <h3 className=" mt-4 text-lg">{discription}</h3>{' '}
+                </Link>
+                <a className="mt-2" href={githubLink}>
+                    깃허브 바로가기
+                </a>
+                <p className="my-1 cursor-default"> 작업기간: {progressPeriod}</p>
+                <div className="flex items-start mt-2 overflow-wrap-break-word cursor-default">
+                    {skillList.map((skill, i) => {
+                        return (
+                            <h1
+                                key={i}
+                                className="
+                                flex
+                                items-start
+                                mt-2
+                                px-2
+                                py-1
+                                mr-2
+                                rounded-md
+                                bg-sky-200
+                                dark:bg-sky-700
+                                w-30
+                                
+                               
+                                "
+                            >
+                                {skill.name}
+                            </h1>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
