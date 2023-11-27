dd9b52d1ca1484a8558652ec798f5ff9bd22960c
Jangchanyoung <jchy16@gmail.com>
2023-11-28 05:05:18 +0900

    MODIFY: html lang ko 변경 (SEO 개선 사항)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="ko">
             <body>
                 <Providers>
                     <div className="bg-primary flex flex-col item-center justify-center">
