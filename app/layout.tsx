31e57bb2bf0b6a05757371532b608e4e64de2777
Jangchanyoung <jchy16@gmail.com>
2023-11-28 05:09:01 +0900

    MODIFY: html lang en 재변경 (vercel 서버 위치 관련 SEO 점수 하락)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="ko">
+        <html lang="en">
             <body>
                 <Providers>
                     <div className="bg-primary flex flex-col item-center justify-center">
