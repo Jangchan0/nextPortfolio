572793dffb4404af6da047c2d834460947a6d2ee
jangchanyoung <jchy16@gmail.com>
2023-11-28 10:39:56 +0900

    wip

--- app/projects_/page.js
+++ app/projects_/page.js
@@ -35,33 +35,6 @@ export default async function Projects() {
     );
 }
 
-// NextJs 12 버전까지 지원함 (getStaticProps)
-
-// export async function getServerSideProps({ params, preview, previewData }) {
-//     const options = {
-//         method: 'POST',
-//         headers: {
-//             accept: 'application/json',
-//             'Notion-Version': '2022-06-28',
-//             'content-type': 'application/json',
-//             authorization: `Bearer ${TOKEN}`,
-//         },
-//         body: JSON.stringify({ page_size: 100 }),
-//     };
-
-//     const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
-//     const projects = await res.json();
-
-//     const projectsName = projects.results.map((aProject) => {
-//         return aProject.properties.이름.title[0]?.text.content;
-//     });
-
-//     return {
-//         props: { projectsName },
-//         revalidate: 1, // 데이터 변경이 있으면 갱신 1초 마다 - 갱신 주기 설정 가능
-//     };
-// }
-
 async function getData() {
     const options = {
         method: 'POST',
