53d42c4e812ebeb7b8adbd6b2137836e61d07a8b
jangchanyoung <jchy16@gmail.com>
2023-11-30 00:58:10 +0900

    MODIFY: 타이핑 로직코드 컴포넌트화

---
+++ components/home/TypingAnimation.js
@@ -0,0 +1,64 @@
+// TypingAnimation.js
+'use client';
+
+import { useState, useEffect } from 'react';
+
+const TypingAnimation = ({ toRotate }) => {
+    const [text, setText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [isTypingFin, setIsTypingFin] = useState(false);
+    const [tinkleState, setTinkleState] = useState(true);
+    const [delta, setDelta] = useState(200 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [text]);
+
+    const tinkle = () => {
+        isTypingFin ? setTinkleState(!tinkleState) : setTinkleState(true);
+    };
+    setTimeout(tinkle, 700, tinkleState);
+
+    const tick = () => {
+        let fullText = toRotate;
+        let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updateText);
+
+        if (updateText.length === toRotate.length) {
+            setIsTypingFin(true);
+            tinkle();
+        } else {
+            setIsTypingFin(false);
+        }
+
+        if (isDeleting) {
+            setDelta((prevDelta) => prevDelta / 2);
+        }
+        if (!isDeleting && updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updateText === '') {
+            setIsDeleting(false);
+            setDelta(200 - Math.random() * 100);
+        }
+    };
+
+    return (
+        <>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">
+                {text}
+                {tinkleState && '⎢'}
+            </h1>
+        </>
+    );
+};
+
+export default TypingAnimation;
