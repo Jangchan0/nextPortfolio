e11bea4786f432edb98226bdaf72584aea3e5f74
jangchanyoung <jangchan-young@jangchan-yeong-ui-MacBookAir.local>
2023-11-29 19:22:19 +0900

    FEAT: 타이핑 효과 깜박임 추가

--- components/home/hero.js
+++ components/home/hero.js
@@ -7,9 +7,17 @@ import Link from 'next/link';
 function Hero() {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ['주니어 프론트엔드 개발자 장찬영입니다'];
+    const bar = '⎢';
+    const toRotate = '주니어 프론트엔드 개발자 장찬영입니다';
+    const [tinkleState, setTinkleState] = useState(true);
+
+    const tinkle = () => {
+        isDeleting ? setTinkleState(!tinkleState) : setTinkleState(true);
+    };
+    setTimeout(tinkle, 600, tinkleState);
+
     const [text, setText] = useState('');
-    const [delta, setDelta] = useState(200 - Math.random() * 100);
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
     useEffect(() => {
@@ -23,8 +31,7 @@ function Hero() {
     }, [text]);
 
     const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[0];
+        let fullText = toRotate;
         let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
         setText(updateText);
@@ -38,7 +45,7 @@ function Hero() {
         } else if (isDeleting && updateText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
-            setDelta(200 - Math.random() * 100);
+            setDelta(300 - Math.random() * 100);
         }
     };
 
@@ -46,25 +53,26 @@ function Hero() {
         <>
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <div style={{ height: '130px' }}>
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">안녕하세요</h1>
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">안녕하세요 !</h1>
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 flex">
                         {text}
+                        <h1 style={tinkleState ? { display: '' } : { display: 'none' }}>{bar}</h1>
                         {/* <br className="hidden lg:inline-block" /> */}
                     </h1>
                 </div>
 
-                <p className="mb-8 leading-relaxed">
+                {/* <p className="mb-8 leading-relaxed">
                     안녕하세요! 저는 주니어 프론트엔드 개발자로, React를 사용하며 웹 애플리케이션을 개발하고 있습니다.
                     끈끈한 협업과 도전적인 학습을 통해 성장하는 것을 즐깁니다. <br />
                     문제 해결과 효율적인 코드 작성에 주력하여 사용자들에게 뛰어난 웹 경험을 제공하고자 노력하고
                     있습니다. 더 나은 사용자 인터페이스와 새로운 기술에 대한 열정으로 프로젝트에 기여하고 싶습니다.{' '}
                     <br />
                     함께 일하고 성장하는 기회를 기다리고 있습니다.
-                </p>
+                </p> */}
                 <div className="flex justify-center">
                     <button>
                         <Link href="/projects_" className="btn-project">
-                            프로젝트 보러가기
+                            Show my project &nbsp; 〉〉
                         </Link>
                     </button>
                 </div>
