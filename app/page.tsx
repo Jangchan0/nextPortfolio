f7c61b407c03f035aaab92c04efba71a6163a5fe
jangchanyoung <jangchan-young@jangchan-yeong-ui-MacBookAir.local>
2023-12-01 01:55:43 +0900

    MODIFY: 메인, 프로젝트 페이지 병합 중 (메인페이지 100vh 적용)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/home/hero';
+import Projects from './projects_/page';
 
 export default function Home() {
     return (
         <div>
             <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div className=" container mx-auto  px-5 py-24 md:flex-row  items-center">
                     <Hero />
+                    <Projects />
                 </div>
             </section>
         </div>
