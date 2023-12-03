282e50a9875a2a9631b734fa835d22812e9db28f
jangchanyoung <jangchan-young@jangchan-yeong-ui-MacBookAir.local>
2023-12-03 22:15:05 +0900

    FEAT: contact 테마별 이미지 적용

--- components/contact/page.tsx
+++ components/contact/page.tsx
@@ -2,12 +2,16 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import developerImg from '../../public/developerProfile.png';
+import developerImgDark from '../../public/developerProfileDark.png';
+import { useTheme } from 'next-themes';
 
 function Contact() {
+    const { theme, setTheme } = useTheme();
+
     interface FormData {
         name: string;
         title: string;
-        message: string;
+        message: string | number;
     }
 
     const [formData, setFormData] = useState<FormData>({
@@ -30,21 +34,25 @@ function Contact() {
         // 이 부분을 서버와의 통신으로 변경해야합니다.
     };
 
+    const developerProfile = theme === 'light' ? developerImgDark : developerImg;
+
     return (
         <>
-            <div className="flex grid-cols-2 justify-center item-center bg-slate-600">
+            <div className="flex grid-cols-2 justify-center item-center bg-primary rounded-md py-10">
                 <div className="contactImg w-1/2">
                     <h2 className="text-3xl font-bold sm:text-6xl justify-center flex my-9">Contact</h2>
                     <div className="developerImg flex justify-center">
-                        <Image src={developerImg} alt="developer profile" width={350} height={350} />
+                        <Image src={developerProfile} alt="developer profile" width={350} height={350} />
                     </div>
                 </div>
                 <div className="contactContants w-1/2 flex">
                     <div className="container mx-auto p-4 flex justify-center items-center">
-                        {/* <h2 className="text-3xl font-semibold mb-4">Contact</h2> */}
                         <form className="container max-w-md flex flex-col ">
                             <div className="mb-8">
-                                <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+                                <label
+                                    htmlFor="name"
+                                    className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2"
+                                >
                                     Name
                                 </label>
                                 <input
@@ -53,11 +61,14 @@ function Contact() {
                                     name="name"
                                     value={formData.name}
                                     onChange={handleChange}
-                                    className="w-full border rounded p-2 bg-slate-400"
+                                    className="w-full border rounded p-2 bg-slate-200 dark:bg-slate-400"
                                 />
                             </div>
                             <div className="mb-8">
-                                <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
+                                <label
+                                    htmlFor="title"
+                                    className="block text-gray-700 dark:text-gray-100  text-sm font-bold mb-2"
+                                >
                                     Title
                                 </label>
                                 <input
@@ -66,11 +77,14 @@ function Contact() {
                                     name="title"
                                     value={formData.title}
                                     onChange={handleChange}
-                                    className="w-full border rounded p-2  bg-slate-400"
+                                    className="w-full border rounded p-2  bg-slate-200 dark:bg-slate-400"
                                 />
                             </div>
                             <div className="mb-8">
-                                <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+                                <label
+                                    htmlFor="message"
+                                    className="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2"
+                                >
                                     Message
                                 </label>
                                 <textarea
@@ -78,13 +92,14 @@ function Contact() {
                                     name="message"
                                     value={formData.message}
                                     onChange={handleChange}
-                                    className="w-full border rounded p-2  bg-slate-400"
+                                    className="w-full h-28 border rounded p-2  bg-slate-200 dark:bg-slate-400"
+                                    style={{ resize: 'none' }}
                                 />
                             </div>
                             <button
                                 type="button"
                                 onClick={handleSend}
-                                className="bg-blue-500 text-white p-2 rounded mt-6"
+                                className="bg-blue-500 text-white p-2 rounded mt-4"
                             >
                                 Send
                             </button>
