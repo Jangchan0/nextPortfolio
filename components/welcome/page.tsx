2fe138a94fc084d15032e973ed4494e7bb126d4e
jangchanyoung <jchy16@gmail.com>
2023-12-05 23:17:39 +0900

    FEAT: 페이지 렌더링 인삿말 토스트 추가

---
+++ components/welcome/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const WelcomeToast = () => {
+    useEffect(() => {
+        toast.info('안녕하세요! 프론트엔드 장찬영입니다!', {
+            icon: '😄',
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 2500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+        });
+    }, []);
+
+    return <ToastContainer />;
+};
+
+export default WelcomeToast;
