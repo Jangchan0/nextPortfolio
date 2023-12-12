615379d32c5fccd797f0a47b889a4639fc89951f
jangchanyoung <jchy16@gmail.com>
2023-12-12 20:50:56 +0900

    FEAT: background threeJs 고정 성공!(스크롤 대비 카메라 무빙 값 조절 필요)

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -3,7 +3,7 @@ import DarkModeToggleBtn from '../home/darkModeToggleBtn';
 
 export default function Header() {
     return (
-        <header className="text-gray-600 body-font">
+        <header className="text-gray-600 body-font z-1 ">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg
--- components/three/Background.js
+++ components/three/Background.js
@@ -10,14 +10,15 @@ const BackgroundThree = () => {
 
     useEffect(() => {
         const scene = new THREE.Scene(); // 화면 씬 생성
-        const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000); // 카메라 생성
+        const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000); // 카메라 생성
 
-        camera.position.set(5, 0, 10); // 카메라 위치
-        camera.lookAt(new THREE.Vector3(0, 5, 0)); // 카메라가 바라보는 위치
+        camera.position.set(2, 0, 10); // 카메라 위치
+        camera.lookAt(new THREE.Vector3(0, 0, 0)); // 카메라가 바라보는 위치
 
         const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, antialias: true }); //
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setClearColor(0x000000, 0);
+        renderer.setSize(window.innerWidth, window.innerHeight); // 초기 크기 설정
 
         const ambientLight = new THREE.AmbientLight(0xffffff, 1.0); // 조명 색상, 강도 설정
         scene.add(ambientLight); // 화면 씬에 더하다.
@@ -29,9 +30,19 @@ const BackgroundThree = () => {
             scene.add(model);
         });
 
+        const handleResize = () => {
+            const newWidth = window.innerWidth;
+            const newHeight = window.innerHeight;
+
+            camera.aspect = newWidth / newHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(newWidth, newHeight);
+        };
+
         const handleScroll = () => {
-            camera.position.y = 2 + scrollRef.current / 100; // Adjust the factor as needed
-            camera.lookAt(new THREE.Vector3(0, 0.5 - scrollRef.current / 100, 0));
+            camera.position.y = 2 - scrollRef.current / 100; // Adjust the factor as needed
+            // camera.lookAt(new THREE.Vector3(0, 0.5 - scrollRef.current / 100, 0));
         };
 
         const animate = () => {
@@ -42,6 +53,8 @@ const BackgroundThree = () => {
 
         animate();
 
+        window.addEventListener('resize', handleResize);
+
         window.addEventListener('scroll', () => {
             scrollRef.current = window.scrollY;
             handleScroll();
@@ -50,11 +63,12 @@ const BackgroundThree = () => {
         return () => {
             loader.dispose();
             renderer.dispose();
+            window.removeEventListener('resize', handleResize);
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
-    return <canvas className="w-full h-full absolute z-0" ref={canvasRef} />;
+    return <canvas className="fixed top-0 z-0" ref={canvasRef} />;
 };
 
 export default BackgroundThree;
