0df1ad6445f22ee4d81c100746e88fbf96b5a0ce
jangchanyoung <jchy16@gmail.com>
2023-12-19 19:00:03 +0900

    MODIFY: footer 반응형 보수

--- components/footer/page.tsx
+++ components/footer/page.tsx
@@ -6,17 +6,17 @@ import Instagram from '../../public/svg/Instagram';
 export default function Footer() {
     return (
         <>
-            <footer className="body-font bg-primary z-10 ">
+            <footer className="body-font bg-primary ">
                 <div className="container flex item-center justify-between mx-auto">
-                    <div className=" px-5 py-6  flex items-center sm:flex-row flex-col">
+                    <div className=" px-4 py-6  flex items-center sm:flex-row flex-col">
                         <Link
                             href="/"
                             className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
                         >
-                            <span className="ml-3 text-xl">장찬영 포트폴리오</span>
+                            <span className="ml-3 lg:text-xl md:text-lg sm:text-xs">장찬영 포트폴리오</span>
                         </Link>
-                        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-                            © 2023 Frontend —
+                        <p className="lg:text-sm md:text-sm sm:text-xs text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+                            © 2024 Frontend —
                             <a
                                 href="https://github.com/Jangchan0"
                                 rel="noopener noreferrer"
@@ -27,7 +27,7 @@ export default function Footer() {
                             </a>
                         </p>
                     </div>
-                    <div className="flex items-center gap-3">
+                    <div className="flex items-center gap-3 mr-2">
                         <div
                             className="inline-flex transition duration-300 transform  hover:scale-110
                              hover:shadow-sm sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
